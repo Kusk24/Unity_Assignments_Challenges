f148710d5a890cd3dd47e5d94b81b898c062e286
Kusk24 <winyumg2003a@gmail.com>
2025-07-26 11:35:02 +0700

    Finished challenge 3

--- Challenge 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Challenge 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerControllerX : MonoBehaviour
 {
@@ -16,6 +17,10 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip jumpSound;
+    private bool isLowEnough;
+    public float topBound = 15f; // Set the ceiling height limit
+    private bool wasSpacePressed = false; // Track if space was pressed in previous frame
 
 
     // Start is called before the first frame update
@@ -33,11 +38,35 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        // Check if player is below the ceiling/top boundary
+        if (transform.position.y < topBound)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            isLowEnough = true;
         }
+        else
+        {
+            isLowEnough = false;
+            transform.position = new Vector3(transform.position.x, topBound, transform.position.z);
+        }
+        // Check if space is currently pressed
+        bool isSpacePressed = Input.GetKey(KeyCode.Space);
+        
+        // While space is pressed and player is low enough, float up
+        if (isSpacePressed && !gameOver && isLowEnough)
+        {
+            // Only play jump sound when space is first pressed (not held)
+            if (!wasSpacePressed)
+            {
+                playerAudio.PlayOneShot(jumpSound, 1.0f);
+            }
+            if (!(transform.position.y == topBound))
+            {
+                playerRb.AddForce(Vector3.up * floatForce);
+            }
+        }
+        
+        // Update the previous frame's space key state
+        wasSpacePressed = isSpacePressed;
     }
 
     private void OnCollisionEnter(Collision other)
@@ -50,7 +79,7 @@ public class PlayerControllerX : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
-        } 
+        }
 
         // if player collides with money, fireworks
         else if (other.gameObject.CompareTag("Money"))
@@ -59,6 +88,12 @@ public class PlayerControllerX : MonoBehaviour
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
 
+        }
+
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce * 10);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
 
     }
