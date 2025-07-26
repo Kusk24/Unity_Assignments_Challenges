0298db7ebda4eb875a131e12eab1980143c95276
Kusk24 <winyumg2003a@gmail.com>
2025-07-26 10:44:36 +0700

    Added Challenge 2 and Initialized Challenge 3

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 0.25f;
+    public GameObject dogPrefab;
+
+    public float horizontalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+
+        if (transform.position.x > 27)
+        {
+            transform.position = new Vector3(27, transform.position.y, transform.position.z);
+        } else if (transform.position.x < -42) {
+            transform.position = new Vector3(-42, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.back * horizontalInput * speed);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Unity_Assignments_Challenges
 This is the assignment aka challenges from Game Design Development Course, Starting from Challenge 2
+
+Challenge 1 -> https://github.com/Kusk24/Challenge-1-Game-App
